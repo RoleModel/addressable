06bff0003d5aee26fe0fec1061e0a1576788687f
Caleb Woods <calebawoods@gmail.com>
2015-01-30 14:36:48 -0500

    Add Angular module if angular is defined

--- dist/addressable.js
+++ dist/addressable.js
@@ -1,5 +1,14 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Addressable=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = require('./lib/addressable');
+var Addressable = require('./lib/addressable');
+
+if (typeof angular !== 'undefined') {
+  angular.module('addressable', []);
+  angular.module('addressable').factory('Addressable', [function () {
+    return Addressable;
+  }]);
+}
+
+module.exports = Addressable;
 
 },{"./lib/addressable":2}],2:[function(require,module,exports){
 'use strict';
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('./lib/addressable');
+var Addressable = require('./lib/addressable');
+
+if (typeof angular !== 'undefined') {
+  angular.module('addressable', []);
+  angular.module('addressable').factory('Addressable', [function () {
+    return Addressable;
+  }]);
+}
+
+module.exports = Addressable;
